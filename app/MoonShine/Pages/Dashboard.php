37d4ea3f13fdd512742e932e9bb5d265f198e025
galyym <ksgalym@mail.ru>
2025-03-15 01:03:37 +0500

    fix: метрика

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use App\Models\CertificateRequest;
+use Google\Service\CloudKMS\Certificate;
 use MoonShine\Laravel\Pages\Page;
 use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Metrics\Wrapped\ValueMetric;
 
 class Dashboard extends Page
 {
@@ -29,6 +32,15 @@ class Dashboard extends Page
      */
     protected function components(): iterable
 	{
-		return [];
+		return [
+            ValueMetric::make('Заявки')
+                ->value(fn() => CertificateRequest::all()->count()),
+            ValueMetric::make('Количество заявок за сегодня')
+                ->value(fn() => CertificateRequest::where('created_at', '>=', now()->startOfDay())->count()),
+            ValueMetric::make('Количество заявок за неделю')
+                ->value(fn() => CertificateRequest::where('created_at', '>=', now()->startOfWeek())->count()),
+            ValueMetric::make('Количество заявок за месяц')
+                ->value(fn() => CertificateRequest::where('created_at', '>=', now()->startOfMonth())->count())
+        ];
 	}
 }
